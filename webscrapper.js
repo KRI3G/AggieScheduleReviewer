01986a4b243fad3d8caa57f40e383a8d17294e62
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2024-09-28 11:38:55 -0600

    stuff done

--- webscrapper.js
+++ webscrapper.js
@@ -1,48 +1,72 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-const scrape = (async (dept, number) => {
+const scrape = (async (dept, section) => {
     //example: 
     //const dept = "ENGR";
     //const number = 102;
-
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    await page.goto(`https://anex.us/grades/?dept=${dept}&number=${number}`);
+    await page.goto(`https://anex.us/grades/?dept=${dept}&number=${section}`);
+    try{
 
-    // Wait for the table to load if it is loaded dynamically
-    await page.waitForSelector('tr');
-    await page.waitForSelector('td');
+        // Wait for the table to load if it is loaded dynamically
 
-    const data = [];
-    // Grab the HTML content of the table
-    const header_row = await page.$eval('#tableDiv thead tr', el => el.innerHTML);
-    const headers = header_row.split("</td><td>");
-    let n = headers.length;
-    headers[0] = headers[0].substring(4);
-    headers[n-1] = headers[n-1].substring(0,headers[n-1].length-5);
-    //data.push(headers);
-    //console.log(headers);
-    
-    const table = await page.$eval('#tableDiv tbody', el => el.innerHTML);
-    const rows = table.split("</tr><tr>");
-    let n2 = rows.length;
-    rows[0] = rows[0].substring(4);
-    rows[n2-1] = rows[n2-1].substring(0,rows[n2-1].length-5);
-    //console.log(rows);
-    rows.forEach((row_pre, index) => {
-        const row = row_pre.split("</td><td>");
-        let n3 = row.length;
-        row[0] = row[0].substring(4);
-        row[n3-1] = row[n3-1].substring(0,row[n3-1].length-5);
+        const chart_check = await page.$eval('#chartDiv', el => el.innerHTML)
+        if(chart_check.includes("No data returned. Is your input correct?")){
+            throw new Error(`No data for section: ${section} in this department: ${dept}.`);
+        }
+        await page.waitForSelector('tr');
+        await page.waitForSelector('td');
 
-        const merge = headers.reduce((obj, key, index) => {
-            obj[key] = row[index];
-            return obj;
-        }, {});
-        data.push(merge);
-        console.log(merge);
+        const data = [];
+        // Grab the HTML content of the table
+        const header_row = await page.$eval('#tableDiv thead tr', el => el.innerHTML);
+        const headers = header_row.split("</td><td>");
+        let n = headers.length;
+        headers[0] = headers[0].substring(4);
+        headers[n-1] = headers[n-1].substring(0,headers[n-1].length-5);
+        //data.push(headers);
+        //console.log(headers);
+        
+        const table = await page.$eval('#tableDiv tbody', el => el.innerHTML);
+        const rows = table.split("</tr><tr>");
+        let n2 = rows.length;
+        rows[0] = rows[0].substring(4);
+        rows[n2-1] = rows[n2-1].substring(0,rows[n2-1].length-5);
+        //console.log(rows);
+        rows.forEach((row_pre, index) => {
+            const row = row_pre.split("</td><td>");
+            let n3 = row.length;
+            row[0] = row[0].substring(4);
+            row[n3-1] = row[n3-1].substring(0,row[n3-1].length-5);
+
+            const merge = headers.reduce((obj, key, index) => {
+                obj[key] = row[index];
+                return obj;
+            }, {});
+            data.push(merge);
+        });
+
+        const json_object_pre = data.reduce((object, item, index) => {
+            object[index] = item;
+            return object;
+        });
+        const json_object = JSON.stringify(json_object_pre, null, 2);
+
+        fs.writeFile('anex_data.json', json_object, (err) => {
+            if(err) { 
+                console.error("Error writing JSON object to file", err);
+            }else{
+                console.log("JSON data successfully written to file!");
+            }
     });
-
-    browser.close();
+    }catch(error){
+        console.error(`No data for section: ${section} in this department: ${dept}.`);
+    }finally {
+        await browser.close();
+        console.log("Broswer closed.");
+    }
 });
-scrape("ENGR", 102);
+
+scrape("MEEN", 210);
