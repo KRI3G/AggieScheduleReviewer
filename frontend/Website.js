8aadc4bca71fed6a2cdc44ab109cdf11ebe14896
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2024-09-28 19:38:39 -0600

    i do be realling

---
+++ frontend/Website.js
@@ -0,0 +1,9 @@
+const pdf_form = document.querySelector("#pdf-form");
+
+pdf_form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(){
+    pdf_form.style.display = "none";
+    
+}
+
