d645a7c7bc2095499944d0f94e0ed5775d73e911
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2024-09-28 18:45:21 -0600

    pushers!

--- backend/anex_schedule_parser.js
+++ backend/anex_schedule_parser.js
@@ -1,6 +1,5 @@
 const fs = require("fs").promises;
-const fetch = require('node-fetch')
-const scraper = require("./webscrapper.js");
+const fetch = require('node-fetch');
 
 async function loadJSON(filePath) {
   try{
@@ -17,7 +16,8 @@ async function main(){
   const schedule_data = schedule_data_pre["data"];
   const full_data = {};
 
-  for(class_data of schedule_data){
+  for(let i = 0;i<schedule_data.length;i++){
+    const class_data = schedule_data[i];
     const dept = class_data["class"]["dept"];
     const number = class_data["class"]["num"];
     const section = class_data["class"]["section"];
@@ -31,9 +31,62 @@ async function main(){
       const params = new URLSearchParams();
       params.append('dept', dept);
       params.append('number', number)
+      //console.log('fetch!');
       const response = await fetch('https://anex.us/grades/getData/?', {method: 'POST', body: params, headers: {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"}})
-      const anex_data = await response.json()
-      console.log(anex_data)
+      response.json().then((anex_data) => {
+        let gpa_sum = 0;
+        let classes = 0;
+        let a_sum = 0; 
+        let n_students = 0;
+        const analytics = {
+          "Average_GPA": "", //gpa_sum/classes
+          "A_percentage": "", //a_sum / n_students
+          "Total_Students_Taught": "",
+
+        };
+        const past_classes = []; //2d array for all classes
+        const filters = ["A","B","C","D","F","I","Q","S","U","X"];
+        console.log("searching for prof: " + instructor + ` in class ${dept}-${number}-${section}`);
+        //console.log(anex_data);
+        const new_list = anex_data.classes.filter(data => instructor == data.prof);
+        if(new_list.length == 0){
+          console.log('zero length');
+        }
+        for(professor_class_analytics of new_list){
+          //alternatively throw into hm and query that way, probably way faster
+          //console.log(instructor)
+          //console.log(professor_class_analytics["Prof"])
+          //console.log("matched: ", instructor, " vs ", professor_class_analytics["prof"]);
+          gpa_sum += Number(professor_class_analytics["gpa"]);
+          a_sum += Number(professor_class_analytics["A"]);
+          classes++;
+          for(const filter of filters){
+            n_students += Number(professor_class_analytics[filter]);
+          }
+            past_classes.push(professor_class_analytics);
+          }
+          analytics["Average_GPA"] = gpa_sum/classes;
+          analytics["A_percentage"] = (a_sum/n_students) * 100;
+          analytics["Total_Students_Taught"] = n_students;
+          const object = {
+            "grade_analytics":{
+              "data_available": true,
+              "data" : analytics,
+              "past_classes" : past_classes,
+            }
+          }
+          full_data[full] = object;
+          //full_data.push(past_classes);
+        }).catch((error) => {
+          const object = {
+            "analytics":{
+              "data_available": false
+            }
+          }
+          Object.assign(full_data, object);
+          console.log("error while grabbing JSON: ", error);
+        });
+      //console.log(anex_data)
       /*
       {
       "Year": "2024",
@@ -53,46 +106,6 @@ async function main(){
       "X": "0"
     },
       */
-      let gpa_sum = 0;
-      let classes = 0;
-      let a_sum = 0; 
-      let n_students = 0;
-      const analytics = {
-        "Average_GPA": "", //gpa_sum/classes
-        "A_percentage": "", //a_sum / n_students
-        "Total_Students_Taught": "",
-
-      };
-      const past_classes = []; //2d array for all classes
-      const filters = ["A","B","C","D","F","I","Q","S","U","X"];
-      console.log("searching for prof: " + instructor + ` in class ${dept}-${number}-${section}`);
-      //console.log(anex_data);
-      for(professor_class_analytics of anex_data.classes){
-        //alternatively throw into hm and query that way, probably way faster
-        console.log(instructor)
-        console.log(professor_class_analytics["Prof"])
-        if(instructor != professor_class_analytics["Prof"]){
-          //console.log("skipping: ", instructor, " vs ", professor_class_analytics["Prof"]);
-          continue;
-        }
-        console.log("matched: ", instructor, " vs ", professor_class_analytics["Prof"]);
-        gpa_sum += Number(professor_class_analytics["GPA"]);
-        a_sum += Number(professor_class_analytics["A"]);
-        classes++;
-        for(const filter of filters){
-          n_students += Number(professor_class_analytics[filter]);
-        }
-        past_classes.push(professor_class_analytics);
-      }
-      analytics["Average_GPA"] = gpa_sum/classes;
-      analytics["A_percentage"] = (a_sum/n_students) * 100;
-      analytics["Total_Students_Taught"] = n_students;
-      const object = {
-        "analytics" : analytics,
-        "past_classes" : past_classes,
-      }
-      full_data[full] = object;
-      //full_data.push(past_classes);
     }catch(error){
       console.error("Error processing class data: ", error);
     }
